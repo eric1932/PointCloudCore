bbc2a2643efda08075280660d9cfa5ae975c30b8
Eric Liu <eric1932@users.noreply.github.com>
2023-02-09 23:53:49 -0800

    Optimize ElemRenderer

--- Runtime/Scripts/ElemRenderer.cs
+++ Runtime/Scripts/ElemRenderer.cs
@@ -5,20 +5,6 @@ public class ElemRenderer : MonoBehaviour
 {
     Mesh mesh;
 
-    private void Awake()
-    {
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     public void UpdateMesh(float[] arrVertices, byte[] arrColors, int nPointsToRender, int nPointsRendered)
     {
         int nPoints;
@@ -29,16 +15,19 @@ public class ElemRenderer : MonoBehaviour
             nPoints = System.Math.Min(nPointsToRender, (arrVertices.Length / 3) - nPointsRendered);
         nPoints = System.Math.Min(nPoints, 65535);
 
+        float[] normalizedColors = System.Array.ConvertAll(arrColors, x => x / 256f);
+
         Vector3[] points = new Vector3[nPoints];
-        int[] indices = new int[nPoints];
         Color[] colors = new Color[nPoints];
+        int[] indices = new int[nPoints];
         for (int i = 0; i < nPoints; i++)
         {
             int ptIdx = 3 * (nPointsRendered + i);
 
             points[i] = new Vector3(arrVertices[ptIdx + 0], arrVertices[ptIdx + 1], -arrVertices[ptIdx + 2]);
+            //colors[i] = new Color((float)arrColors[ptIdx + 0] / 256.0f, (float)arrColors[ptIdx + 1] / 256.0f, (float)arrColors[ptIdx + 2] / 256.0f, 1.0f);
+            colors[i] = new Color(normalizedColors[ptIdx + 0], normalizedColors[ptIdx + 1], normalizedColors[ptIdx + 2], 1.0f);
             indices[i] = i;
-            colors[i] = new Color((float)arrColors[ptIdx + 0] / 256.0f, (float)arrColors[ptIdx + 1] / 256.0f, (float)arrColors[ptIdx + 2] / 256.0f, 1.0f);         
         }
 
         if (mesh != null)
